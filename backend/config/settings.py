65802470088a4fa75f677b73eba4fd4848511f32
parshv1234 <modiparshv2005@gmail.com>
2025-11-23 12:08:04 +0530

    Added requirements.txt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ef9vvd4rj_5bp$no6ngwa@u1@3bn8)^y+=!zg$rd9$d6bb0sj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -129,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-CORS_ALLOW_ALL_ORIGINS = True # Allow React to talk to Django
+CORS_ALLOW_ALL_ORIGINS = True # Allow your Vercel frontend React to talk to Django backend Render
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
